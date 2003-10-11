b3260ebb2dd38c1e21ee55cc247e577be09bed00
amontresor <>
2003-10-11 21:16:37 +0000

    Committing a file to verify that scripts work

--- peersim/aggregation/FallibleAverageFunction.java
+++ peersim/aggregation/FallibleAverageFunction.java
@@ -23,12 +23,13 @@ import peersim.core.*;
 import peersim.config.*;
 
 /**
- * 
+ *  This class emulates
  *
  *  @author Alberto Montresor
  *  @version $Revision$
  */
-public class FallibleAverageFunction extends AbstractFunction
+public class FallibleAverageFunction 
+extends AbstractFunction
 {
 
 	////////////////////////////////////////////////////////////////////////////
@@ -39,8 +40,9 @@ public class FallibleAverageFunction extends AbstractFunction
 
 	private final static String PAR_STYLE = "style";
 
+
 	////////////////////////////////////////////////////////////////////////////
-	// Fields
+	// Static fields
 	////////////////////////////////////////////////////////////////////////////
 
 	/** Failure probability */
@@ -48,6 +50,18 @@ public class FallibleAverageFunction extends AbstractFunction
 
 	/** True if the failure style is atomic */
 	private static boolean atomic;
+
+	////////////////////////////////////////////////////////////////////////////
+	// Fields
+	////////////////////////////////////////////////////////////////////////////
+
+  /** 
+   * This flag is set to true when a node is created. Node that are created
+   * during an epoch do not partecipate in the aggregation protocol for that
+   * epoch. When the value is re-initialized, nodes are re-created equal.
+   */
+	private boolean justArrived;
+
 
 	////////////////////////////////////////////////////////////////////////////
 	// Constructor
@@ -59,12 +73,14 @@ public class FallibleAverageFunction extends AbstractFunction
 		failureProb = Configuration.getDouble(prefix + "." + PAR_FAILUREPROB);
 		String style = Configuration.getString(prefix + "." + PAR_STYLE, "atomic");
 		atomic = ("atomic".equalsIgnoreCase(style));
+		justArrived = true;
 	}
 
 	public Object clone() throws CloneNotSupportedException
 	{
 		FallibleAverageFunction af = (FallibleAverageFunction) super.clone();
 		af.value = value;
+		af.justArrived = true;
 		return af;
 	}
 
@@ -76,46 +92,99 @@ public class FallibleAverageFunction extends AbstractFunction
 	 * Using a {@link Linkable} protocol choses a neighbor and performs a
 	 * variance reduction step.
 	 */
-	public void nextCycle(Node node, int protocolID)
+	public void nextCycle(Node node, int pid)
 	{
-		// We perform a random test based on the failure probability
-		// In both the atomic and non-atomic case, if this test fails
-		// we do not do nothing here: the first message has been lost.
-		if (CommonRandom.r.nextDouble() >= failureProb)
-		{
-			
-			int linkableID = Protocols.getLink(protocolID);
-			Linkable linkable = (Linkable) node.getProtocol(linkableID);
-			if (linkable.degree() > 0)
-			{
-				Node peer =
-					linkable.getNeighbor(CommonRandom.r.nextInt(linkable.degree()));
+		/* Nodes that have been created during the current epoch do not
+		 * partecipate in the aggregation protocol
+		 */
+		if (justArrived)
+		  return;
+		
+		Node rnode = selectNeighbor(node, pid);
 
-				// XXX quick and dirty handling of failure
-				if (peer.getFailState() != Fallible.OK)
-					return;
+		/* Quick and dirty check whether the selected node is up and running.
+		 * If not, we just ignore the fact.
+		 */
+		if (rnode == null || !rnode.isUp())
+			return;
 
-				FallibleAverageFunction neighbor =
-					(FallibleAverageFunction) peer.getProtocol(protocolID);
-				if (atomic)
-				{
-					// In the atomic case, since we passed the first
-					// test, we may safely perform the exchange
-					double mean = (this.value + neighbor.value) / 2;
-					this.value = mean;
-					neighbor.value = mean;
-				} else
-				{
-					// In the non-atomic case, we perform another test
-					// for the response message. If the message is lost,
-					// we just update the contacted node.
-					double mean = (this.value + neighbor.value) / 2;
-					neighbor.value = mean;
-					if (CommonRandom.r.nextDouble() >= failureProb)
-						this.value = mean;
-				}
-			}
+		FallibleAverageFunction rpeer = 
+			(FallibleAverageFunction) rnode.getProtocol(pid);
+		
+		/* Sends an exchange request to the remote node that has
+		 * been selected. To simulate message losses, we perform
+		 * a random test on failure probability. In case of
+		 * communication failure, the exchange request is not
+		 * delivered.
+		 */
+		if (failureProb == 0.0 || CommonRandom.r.nextDouble() >= failureProb) {
+			rpeer.deliverRequest(rnode, this, getValue());
+		} 
+		
+		/* Here, we should wait for a response. Since we are
+		 * using a single-threaded simulation, we let the
+		 * remote peer to invoke deliverResponse on this
+		 * protocol object. So, the waiting section of the
+		 * code is not represented.
+		 */
+	}
+	
+	/**
+	 * 
+	 * @param lnode the node that hosts this protocol
+	 * @param rpeer the remote protocol that performed this invocation
+	 * @param rvalue the value stored at the remote protocol
+	 */
+	protected void deliverRequest(Node lnode, FallibleAverageFunction rpeer, 
+		double rvalue)
+	{
+		/* Nodes that have been created during the current epoch do not
+		 * partecipate in the aggregation protocol
+		 */
+		if (justArrived)
+			return;
+
+		double lvalue = this.value;
+		this.value = (lvalue + rvalue)/2;
+		/* If the failure mode is atomic, we send back a response.
+		 * If the failure mode is not atomic, we perform
+		 * a random test on failure probability. In case of
+		 * communication failure, the exchange response is not
+		 * delivered.
+		 */
+		if (failureProb == 0.0 || atomic || 
+		    CommonRandom.r.nextDouble() >= failureProb) {
+			rpeer.deliverResponse(lvalue);
 		}
 	}
+	
+	/**
+	 * 
+	 * @param rvalue the value stored at the remote protocol
+	 */
+	protected void deliverResponse(double rvalue)
+	{
+		/* Here, we do not check whether justArrived is true or false.
+		 * The reason for this is that in our simulation, no nodes
+		 * send a response without having sent a request. In the
+		 * reality, every node can easily discard responses for which
+		 * it has not sent any request. 
+		 */
+		 
+		/* Update the value */
+		this.value = (this.value + rvalue)/2;
+	}
+
+  protected Node selectNeighbor(Node node, int pid)
+  {
+		int linkableID = Protocols.getLink(pid);
+		Linkable linkable = (Linkable) node.getProtocol(linkableID);
+		Node rnode = null;
+		if (linkable.degree() > 0) {
+			rnode =
+				linkable.getNeighbor(CommonRandom.r.nextInt(linkable.degree()));
+		}
+		return rnode; 
+  }
 
 }

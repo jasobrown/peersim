32d5e1fc9178316744913653155173fba8be5057
jelasity <>
2003-12-17 16:17:02 +0000

    *** empty log message ***

--- peersim/peersim/reports/RandRemoval.java
+++ peersim/peersim/reports/RandRemoval.java
@@ -18,14 +18,25 @@ public class RandRemoval implements Observer {
 // ====================================================================
 
 /** 
-*  String name of the parameter used to select the protocol to operate on
+*  String name of the parameter used to select the protocol to operate on.
 */
 public static final String PAR_PROT = "protocol";
+
+// XXX remove side effect
+/** 
+* String name of the parameter which defines the number of runs of the
+* iterative removal procedure to get statistics.
+* Look out: if set to larger than 1 then as a side effect the overlay
+* will be shuffled. Defaults to 1.
+*/
+public static final String PAR_N = "n";
 
 /** The name of this observer in the configuration */
 private final String name;
 
 private final int protocolID;
+
+private final int n;
 
 private final GraphAlgorithms ga;
 
@@ -38,6 +49,7 @@ public RandRemoval(String name) {
 
 	this.name = name;
 	protocolID = Configuration.getInt(name+"."+PAR_PROT);
+	n = Configuration.getInt(name+"."+PAR_N);
 	ga = new GraphAlgorithms();
 }
 
@@ -48,24 +60,68 @@ public RandRemoval(String name) {
 
 public boolean analyze() {
 	
-	OverlayGraph og = new OverlayGraph(protocolID);
-	PrefixSubGraph g = new PrefixSubGraph(og);
-	IncrementalStats stats = new IncrementalStats();
-	
 	System.out.println(name+":");
 	
-	for( int i=og.size()/2; i>0; i-=og.size()/100)
+	if( n == 1 )
 	{
-		g.setSize(i);
-		Map clst = ga.weaklyConnectedClusters(g);
-		stats.reset();
-		Iterator it = clst.values().iterator();
-		while(it.hasNext())
+		OverlayGraph og = new OverlayGraph(protocolID);
+		PrefixSubGraph g = new PrefixSubGraph(og);
+		IncrementalStats stats = new IncrementalStats();
+	
+		for( int i=og.size()/2; i>0; i-=og.size()/100)
 		{
-			stats.add(((Integer)it.next()).intValue());
+			g.setSize(i);
+			Map clst = ga.weaklyConnectedClusters(g);
+			stats.reset();
+			Iterator it = clst.values().iterator();
+			while(it.hasNext())
+			{
+				stats.add(((Integer)it.next()).intValue());
+			}
+
+			System.out.println(stats);
 		}
 
-		System.out.println(stats);
+		return false;
+	}
+		
+	final int size = OverlayNetwork.size();
+	final int steps = 50; // XXX we assume net size is at least 100
+	IncrementalStats[] maxClust = new IncrementalStats[steps];
+	IncrementalStats[] clustNum = new IncrementalStats[steps];
+	for(int i=0; i<steps; ++i)
+	{
+		maxClust[i] = new IncrementalStats();
+		clustNum[i] = new IncrementalStats();
+	}
+	
+	for(int j=0; j<n; ++j)
+	{
+		OverlayNetwork.shuffle();
+		OverlayGraph og = new OverlayGraph(protocolID);
+		PrefixSubGraph g = new PrefixSubGraph(og);
+		IncrementalStats stats = new IncrementalStats();
+		
+		for( int i=0; i<steps; i++)
+		{
+			g.setSize(size/2-i*(size/100));
+			Map clst = ga.weaklyConnectedClusters(g);
+			stats.reset();
+			Iterator it = clst.values().iterator();
+			while(it.hasNext())
+			{
+				stats.add(((Integer)it.next()).intValue());
+			}
+
+			maxClust[i].add(stats.getMax());
+			clustNum[i].add(clst.size());
+		}
+	}
+	
+	for(int i=0; i<steps; ++i)
+	{
+		System.out.println( maxClust[i].getAverage() + " " +
+			clustNum[i].getAverage() );
 	}
 	
 	return false;

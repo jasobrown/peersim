bad1fda9ce69617aeab1d0e3f30d63fc80416bae
amontresor <>
2010-05-30 15:02:43 +0000

    *** empty log message ***

---
+++ web/pubs/labels.pl
@@ -0,0 +1,613 @@
+# LaTeX2HTML 2008 (1.71)
+# Associate labels original text with physical files.
+
+
+$key = q/cite_castelli07-modeling/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_li2007ehe/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_essa/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_tout2007sbp/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_gecco09/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_pournaras2008ldn/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_castelli08-hyperCBR/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_leitao2007ebt/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_JMB06/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_percom-BradlerKM08/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_JMB04/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_JK06/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_JGKvS04/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_ahmad2007sss/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_pipan2010/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_jin2007uta/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_voulgaris.ccpe.2007/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_icdcit-HarishAAMG07/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_acs/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_ahb07/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_1277225/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_ha07/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_ahb05/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_corr-abs-0712-3980/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_BJ08/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_griffiths2008tai/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_ppsn-LaredoESG08/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_10.1109-HPDC.2006.1652131/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_voulgaris.europar.2005/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_ipps-ChengGM07/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_iciw-WangDW08/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_1194773/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_euram07/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_sirio/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_infocom-GuoWCL06/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_ahmed-eaf/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_bansod2008meb/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_ahmed2007dpm/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_gramoli2008sso/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_gcc-GuoCXLCL05/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_1266898/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_10.1109-FGCN.2007.197/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_selfman08/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_jelasity.tocs.2007/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_comito05/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_MJB04/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_JHS07-saso/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_1364700/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_1306284/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_saso07/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_grizard2007ppn/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_kampffmeyer:igs/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Carra-JSAC/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Carra-TPDS/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_4578935/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_amoretti-ibo/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_RAH08/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_aina-SongLLY08/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_saso09fungi/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_costa09:autonomous/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_semco-JinGB07/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_sun2008emm/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_sss-BaldoniGLQP06/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_JM04/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_ICICSE.2008.17/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_10.1109-ICDCS.2008.37/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_icaris-Trapnell05/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_goerlitz08pints/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_1395350/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_cheng2006ipp/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_skyvalidas2007rri/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_1244133/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_JB05/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_epew-ZhangM08/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_1307141/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_chemo-eccs/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_ideal-OttoO06/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_SWQN07/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_1263667/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_MHJ05-soas/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_mhjab05/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_otm-HidayantoB07/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_1333862/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_bansod2005esr/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_boschetti-bdl/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_bison-taas06/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_opodis-BonnetKR07/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_jin2007tts/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_aims-LesueurMT08/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_ica3pp-RenXW07/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_ISCApdcs-StakhanovaFWC04/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_iscc-BessaCB07/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_ganesh2007pca/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_gpc-WanLXR08/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_JCE07/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_iccS-ZhangWL07/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_ica3pp-WuZWLWY07/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_1343840/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_epew-MelloMF07/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_cops09/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_wise-RangerC05/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_filali2008scb/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_icws-WanghamMFB07/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_GJGS07-wons/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_MJB05/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_aoc2009/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_1161259/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_JMB05/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Mon04/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_meng-emg/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_fu2008ptm/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_MJ08-euram/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_wang2007rtt/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_tout-bsc/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Ahmad06a/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_1339010/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_han-dra/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_guo2007mep/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_voulgaris.thesis.2006/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_voulgaris.iptps.2006/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_1270867/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_evangelos:sda/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_pogkas2008amc/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_1376855/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_JC05-icns/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_klmst08/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_1299749/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_europar-LaredoESCMG08/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_gcc-NiuWS05/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_ha06/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_tnsm/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_guo2008bbk/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_1399337/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_euram08/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Jel05/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_comito06/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_citeulike:3896096/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_1481513/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_ha2008/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_middleware-VoulgarisS07/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_hotp2p08/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_p2p2009/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Leitao-dsn07/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_evoworkshops09/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_icppw-RenXW07/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_voulgaris.jnsm.2005/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_xiao2007atm/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_cmot/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_saso/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_ipdps08/;
+$external_labels{$key} = "$URL/" . q|desc.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
+
+# LaTeX2HTML 2008 (1.71)
+# labels from external_latex_labels array.
+
+
+1;
+
